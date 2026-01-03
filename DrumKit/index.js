16bbc695b51be9c65bfdce8df01f28889293ecad
vijesharumugam <vijeshvijesh2601@gmail.com>
2026-01-03 11:54:12 +0530

    JavaScript Eventlistener learning task - drum Kit

---
+++ DrumKit/index.js
@@ -0,0 +1,43 @@
+const butns = document.querySelectorAll('button');
+function playSound(key){
+ switch(key){
+        case 'w':
+            var audio = new Audio('sounds/tom-1.mp3');
+            audio.play();
+            break;
+        case 'a':
+            var audio = new Audio('sounds/tom-2.mp3');
+            audio.play();
+            break;
+        case 's':
+            var audio = new Audio('sounds/tom-3.mp3');
+            audio.play();
+            break;
+        case 'd':
+            var audio = new Audio('sounds/tom-4.mp3');
+            audio.play();
+            break;
+        case 'j':
+            var audio = new Audio('sounds/snare.mp3');
+            audio.play();
+            break;
+        case 'k':
+            var audio = new Audio('sounds/crash.mp3');
+            audio.play();
+            break;
+        case 'l':
+            var audio = new Audio('sounds/kick-bass.mp3');
+            audio.play();
+            break;  
+ } 
+}
+
+
+for(let i = 0; i < butns.length; i++){
+    butns[i].addEventListener('click', function(){
+        playSound(this.innerHTML);
+    });
+}
+document.addEventListener('keydown', function(event){
+    playSound(event.key);
+});
